7e1ae3f08a0731410dd168d15fdb4b2483d8cac6
Tom Roar Furunes <tom.roar@halloverden.no>
2020-02-20 18:15:38 +0100

    FIX: need to use realpath

--- install.sh
+++ install.sh
@@ -11,10 +11,10 @@ chmod +x $DIR/deploy/05-copy_certs_remote.sh
 chmod +x $DIR/deploy/10-reload_haproxy.sh
 chmod +x $DIR/deploy/15-reload_haproxy_remote.sh
 
-ln -s $DIR/deploy/01-copy_certs.sh $DEPLOY_HOOK_PATH/01-copy_certs.sh
-ln -s $DIR/deploy/05-copy_certs_remote.sh $DEPLOY_HOOK_PATH/05-copy_certs_remote.sh
-ln -s $DIR/deploy/10-reload_haproxy.sh $DEPLOY_HOOK_PATH/10-reload_haproxy.sh
-ln -s $DIR/deploy/15-reload_haproxy_remote.sh $DEPLOY_HOOK_PATH/15-reload_haproxy_remote.sh
+ln -s $(realpath $DIR)/deploy/01-copy_certs.sh $DEPLOY_HOOK_PATH/01-copy_certs.sh
+ln -s $(realpath $DIR)/deploy/05-copy_certs_remote.sh $DEPLOY_HOOK_PATH/05-copy_certs_remote.sh
+ln -s $(realpath $DIR)/deploy/10-reload_haproxy.sh $DEPLOY_HOOK_PATH/10-reload_haproxy.sh
+ln -s $(realpath $DIR)/deploy/15-reload_haproxy_remote.sh $DEPLOY_HOOK_PATH/15-reload_haproxy_remote.sh
 
 cp $DIR/deploy/.env.example $DEPLOY_HOOK_PATH/.env
 
