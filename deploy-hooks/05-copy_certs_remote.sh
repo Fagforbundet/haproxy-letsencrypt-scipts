8e522ca1a62db8ad2534d142b67bfaf5296e2975
Erich Json Fosse <erich@halloverden.no>
2020-06-02 00:11:30 +0200

    EDIT: Indentation

--- deploy-hooks/05-copy_certs_remote.sh
+++ deploy-hooks/05-copy_certs_remote.sh
@@ -2,8 +2,8 @@
 
 # Ensure that the script is run via the barrel script
 if [[ "$(basename -- "$0")" != "00-all_deploy_scripts.sh" ]]; then
-    echo "This script should not be run directly. Run the barrel script instead" >&2
-    exit 1
+  echo "This script should not be run directly. Run the barrel script instead" >&2
+  exit 1
 fi
 
 if [[ -z "$RENEWED_DOMAIN" ]]; then
