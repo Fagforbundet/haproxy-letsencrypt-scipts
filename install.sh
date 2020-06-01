0d36623d56b365e32d2dee6323354c0fdd560406
Erich Json Fosse <erich@halloverden.no>
2020-06-01 22:56:11 +0200

    EDIT: Enclosed variables in quotes, and only symlinking the barrel script

--- install.sh
+++ install.sh
@@ -7,16 +7,15 @@ BASEDIR=$(realpath "$(dirname "$0")")
 
 source "$BASEDIR/.env"
 
-chmod +x $DIR/deploy/01-copy_certs.sh
-chmod +x $DIR/deploy/05-copy_certs_remote.sh
-chmod +x $DIR/deploy/10-reload_haproxy.sh
-chmod +x $DIR/deploy/15-reload_haproxy_remote.sh
+chmod +x "$BASEDIR/deploy/00-all_deploy_scripts.sh"
+chmod +x "$BASEDIR/deploy/01-copy_certs.sh"
+chmod +x "$BASEDIR/deploy/05-copy_certs_remote.sh"
+chmod +x "$BASEDIR/deploy/10-reload_haproxy.sh"
+chmod +x "$BASEDIR/deploy/15-reload_haproxy_remote.sh"
 
-ln -s $(realpath $DIR)/deploy/01-copy_certs.sh $DEPLOY_HOOK_PATH/01-copy_certs.sh
-ln -s $(realpath $DIR)/deploy/05-copy_certs_remote.sh $DEPLOY_HOOK_PATH/05-copy_certs_remote.sh
-ln -s $(realpath $DIR)/deploy/10-reload_haproxy.sh $DEPLOY_HOOK_PATH/10-reload_haproxy.sh
-ln -s $(realpath $DIR)/deploy/15-reload_haproxy_remote.sh $DEPLOY_HOOK_PATH/15-reload_haproxy_remote.sh
+rm -f "$DEPLOY_HOOK_PATH/00-all_deploy_scripts.sh"
+ln -s "$BASEDIR/deploy/00-all_deploy_scripts.sh" "$DEPLOY_HOOK_PATH/00-all_deploy_scripts.sh"
 
-cp $DIR/deploy/.env.example $DEPLOY_HOOK_PATH/.env
+cp -f "$BASEDIR/deploy/.env.example" "$DEPLOY_HOOK_PATH/.env"
 
-nano -c $DEPLOY_HOOK_PATH/.env
+nano -c "$DEPLOY_HOOK_PATH/.env"
