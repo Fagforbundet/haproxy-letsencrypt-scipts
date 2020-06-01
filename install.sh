f3a8db9e2edebf68e29e4d19881cb3607ea41205
Erich Json Fosse <erich@halloverden.no>
2020-06-01 23:37:58 +0200

    ADD: Variable for deploy scripts basedir

--- install.sh
+++ install.sh
@@ -4,14 +4,16 @@ set -e
 set -x
 
 BASEDIR=$(realpath "$(dirname "$0")")
+BASEDIR_DEPLOY_SCRIPTS="$BASEDIR/deploy"
 
 source "$BASEDIR/.env"
 
-chmod +x "$BASEDIR/deploy/00-all_deploy_scripts.sh"
-chmod +x "$BASEDIR/deploy/01-copy_certs.sh"
-chmod +x "$BASEDIR/deploy/05-copy_certs_remote.sh"
-chmod +x "$BASEDIR/deploy/10-reload_haproxy.sh"
-chmod +x "$BASEDIR/deploy/15-reload_haproxy_remote.sh"
+# Ensure all scripts are executable
+chmod +x "$BASEDIR_DEPLOY_SCRIPTS/00-all_deploy_scripts.sh"
+chmod +x "$BASEDIR_DEPLOY_SCRIPTS/01-copy_certs.sh"
+chmod +x "$BASEDIR_DEPLOY_SCRIPTS/05-copy_certs_remote.sh"
+chmod +x "$BASEDIR_DEPLOY_SCRIPTS/10-reload_haproxy.sh"
+chmod +x "$BASEDIR_DEPLOY_SCRIPTS/15-reload_haproxy_remote.sh"
 
 rm -f "$DEPLOY_HOOK_PATH/00-all_deploy_scripts.sh"
 ln -s "$BASEDIR/deploy/00-all_deploy_scripts.sh" "$DEPLOY_HOOK_PATH/00-all_deploy_scripts.sh"
