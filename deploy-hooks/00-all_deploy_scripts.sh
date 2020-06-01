8e34e660f88c8e75875194436251d8be918e2ef5
Erich Json Fosse <erich@halloverden.no>
2020-06-02 00:07:17 +0200

    EDIT: Sourcing scripts

--- deploy-hooks/00-all_deploy_scripts.sh
+++ deploy-hooks/00-all_deploy_scripts.sh
@@ -7,7 +7,7 @@ BASEDIR="$BASEDIR_DEPLOY_SCRIPTS/.."
 
 source "$BASEDIR/.env"
 
-"$BASEDIR_DEPLOY_SCRIPTS/01-copy_certs.sh"
-"$BASEDIR_DEPLOY_SCRIPTS/05-copy_certs_remote.sh"
-"$BASEDIR_DEPLOY_SCRIPTS/10-reload_haproxy.sh"
-"$BASEDIR_DEPLOY_SCRIPTS/15-reload_haproxy_remote.sh"
+source "$BASEDIR_DEPLOY_SCRIPTS/01-copy_certs.sh"
+source "$BASEDIR_DEPLOY_SCRIPTS/05-copy_certs_remote.sh"
+source "$BASEDIR_DEPLOY_SCRIPTS/10-reload_haproxy.sh"
+source "$BASEDIR_DEPLOY_SCRIPTS/15-reload_haproxy_remote.sh"
