e6f3e6e596ae3e25667b49b18f540eb305896d37
Erich Json Fosse <erich@halloverden.no>
2020-06-03 15:19:46 +0200

    EDIT: Updated usage function

--- get-certificate.sh
+++ get-certificate.sh
@@ -7,7 +7,7 @@ BASEDIR_DEPLOY_SCRIPTS="$BASEDIR/deploy-hooks"
 
 source "$BASEDIR/.env"
 
-usage() { echo "Usage: $0 <domain name>" 1>&2; }
+usage() { echo "Usage: $(basename "$0") <domain name>" 1>&2; }
 
 if [[ -z "$1" ]]; then
   usage
